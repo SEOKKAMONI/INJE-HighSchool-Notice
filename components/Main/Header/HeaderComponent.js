3d86ca809d9472ba8085ece07acb1eeca89fdf8a
unknown <sj01048260279@gmail.com>
2022-09-11 23:53:53 +0900

    ADD :: Logo 클릭시 새로고침

--- components/Main/Header/HeaderComponent.js
+++ components/Main/Header/HeaderComponent.js
@@ -4,7 +4,7 @@ import GradeClassView from "../../GradeClassView/GradeClassView";
 function HeaderComponent() {
     return (
         <div className="header_content">
-            <div className="logo">
+            <div className="logo" onClick={() => window.location.reload()}>
                 인제알리미
             </div>
             <GradeClassView />
