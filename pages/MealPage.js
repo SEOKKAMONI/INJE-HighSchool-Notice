8d450b0c89cb562c7bb339faaacf04a897f0c0b4
unknown <sj01048260279@gmail.com>
2022-09-12 20:40:43 +0900

    ADD :: MealPage 레이아웃

--- pages/MealPage.js
+++ pages/MealPage.js
@@ -1,17 +1,24 @@
 import { useState } from 'react';
+import "./MealPage.css";
 
 function MainPage() {
 
     return (
         <div className='lunch-content'>
             <div className='breakfast'>
-
+                <div className='meal-title'>
+                    아침 식사
+                </div>
             </div>
             <div className='lunch'>
-
+                <div className='meal-title'>
+                    점심 식사
+                </div>
             </div>
             <div className='dinner'>
-
+                <div className='meal-title'>
+                    저녁 식사
+                </div>
             </div>
         </div>
     )
