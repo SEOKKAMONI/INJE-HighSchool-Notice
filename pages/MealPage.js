ba9a91c81c95efc65fec7455285144981f79c64c
unknown <sj01048260279@gmail.com>
2022-09-12 21:36:22 +0900

    ADD :: 나이스 급식 API 연결

--- pages/MealPage.js
+++ pages/MealPage.js
@@ -1,23 +1,62 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import "./MealPage.css";
 
 function MainPage() {
 
+    const getMeal = async () => {
+        const json = await (
+            await fetch(
+                `https://open.neis.go.kr/hub/mealServiceDietInfo?KEY=af1ccdd3826b47f89c4acbfc8b3ed12a&Type=json&pIndex=1&pSize=3&ATPT_OFCDC_SC_CODE=E10&SD_SCHUL_CODE=7310084`
+            )
+        ).json();
+        console.log(json.mealServiceDietInfo[1].row)
+    }
+
+    useEffect(() => {
+        getMeal()
+    }, [])
+
     return (
-        <div className='lunch-content'>
+        <div className='meal-content'>
             <div className='breakfast'>
                 <div className='meal-title'>
                     아침 식사
                 </div>
+                <div className='meal-list'>
+                    <ul className='breafast-list'>
+                        <li>떡볶이</li>
+                        <li>떡볶이</li>
+                        <li>떡볶이</li>
+                        <li>떡볶이</li>
+                    </ul>
+                </div>
             </div>
+
             <div className='lunch'>
                 <div className='meal-title'>
                     점심 식사
                 </div>
+                <div className='meal-list'>
+                    <ul className='lunch-list'>
+                        <li>떡볶이</li>
+                        <li>떡볶이</li>
+                        <li>떡볶이</li>
+                        <li>떡볶이</li>
+                    </ul>
+                </div>
             </div>
+            
             <div className='dinner'>
                 <div className='meal-title'>
                     저녁 식사
+                </div>
+                <div className='meal-list'>
+                    <ul className='dinner-list'>
+                        <li>떡볶이</li>
+                        <li>떡볶이</li>
+                        <li>떡볶이</li>
+                        <li>떡볶이</li>
+                    </ul>
                 </div>
             </div>
         </div>
