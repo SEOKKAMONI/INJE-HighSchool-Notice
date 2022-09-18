631288f490f4213599232e63f18e21110a60dca1
unknown <sj01048260279@gmail.com>
2022-09-18 13:59:59 +0900

    ADD :: 파일 다 삭제 - 처음 부터 다시

--- App.js
+++ App.js
@@ -1,43 +1,9 @@
 import "./App.css"
 
-import { useState } from 'react';
-
-import MainPage from './pages/MainPage';
-import ClassSurvey from './routes/ClassSurvey';
-
-import { DataContext } from './context/DataContext';
-import { SurveyContext } from './context/SurveyContext';
-import { LunchNoticeContext } from './context/LunchNoticeContext';
-
-
 export default function App() {
-  const [survey, setSurvey] = useState(false); // Survey Check
-  const [checkLunch, setCheckLunch] = useState(false); // Lunch Page Check
-  const [grade, setGrade] = useState(0); // Grade Data
-  const [Class, setClass] = useState(0); // Class Data
-
-  if (!survey) {
-    return (
-      <div className="container">
-        <DataContext.Provider value={{ grade, setGrade, Class, setClass }}>
-          <SurveyContext.Provider value={{ survey, setSurvey }}>
-            <ClassSurvey />
-          </SurveyContext.Provider>
-        </DataContext.Provider>
-      </div>
-    )
-  }
-  else if (survey) {
-    return (
-      <div className="container">
-        <LunchNoticeContext.Provider value={{ checkLunch, setCheckLunch }}>
-          <DataContext.Provider value={{ grade, setGrade, Class, setClass }}>
-            <SurveyContext.Provider value={{ survey, setSurvey }}>
-              <MainPage />
-            </SurveyContext.Provider>
-          </DataContext.Provider>
-        </LunchNoticeContext.Provider>
-      </div>
-    )
-  }
+  return (
+    <div>
+      
+    </div>
+  )
 }
