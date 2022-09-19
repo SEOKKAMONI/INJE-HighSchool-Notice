1100ea055c16937ab44ecca86ebe7c02957a141f
unknown <sj01048260279@gmail.com>
2022-09-19 12:37:02 +0900

    ADD :: main-content-header - SVG 추가 - ON/OFF 스위치는 디자인 미완이라 완성되면 다시 넣을 예정 - main-content-header 레이아웃

--- components/Main/Main.js
+++ components/Main/Main.js
@@ -1,11 +1,23 @@
 import "./Main.css";
+import arrowSVG from "../../img/chevron.left.svg";
 
 export default function Main() {
     return (
         <div className="main-container">
             {/* 시간표, 급식표 나오는 부분 */}
             <div className="main-content">
-
+                {/* content 부분 header */}
+                <div className="main-content-header">
+                    <div className="content-header__logo">
+                        {/* header 안 내용 */}
+                        <span className="logo__title">
+                            <span className="title__text">
+                                시간표
+                            </span>
+                            <img src={arrowSVG} />
+                        </span>
+                    </div>
+                </div>
             </div>
         </div>
     )
