b2a858175894d972a2762eb283a03c70e8702a48
unknown <sj01048260279@gmail.com>
2022-09-19 20:08:29 +0900

    ADD :: 날짜에 맞게 시간표 요일 강조 - class 삼항 연산자 사용하여 className 관리 - UseState 로 동기화

--- components/Schedule/Schedule.js
+++ components/Schedule/Schedule.js
@@ -1,15 +1,24 @@
 import "./Schedule.css";
 import ScheduleColumn from "../ScheduleColumn/ScheduleColumn";
+import { useState } from "react";
+import { useEffect } from "react";
 
 export default function Schedule() {
+    const [today, setToday] = useState("");
+    const week = new Array('일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일');
+    let DAY = new Date().getDay();
+    useEffect(() => {
+        setToday(week[DAY]);
+    }, [today])
+
     return (
         <div className="schedule-container">
             <div className="schedule-container__header">
-                <span>월</span>
-                <span>화</span>
-                <span>수</span>
-                <span>목</span>
-                <span>금</span>
+                <span className={today == "월요일" ? "header__today" : "header__day"}>월</span>
+                <span className={today == "화요일" ? "header__today" : "header__day"}>화</span>
+                <span className={today == "수요일" ? "header__today" : "header__day"}>수</span>
+                <span className={today == "목요일" ? "header__today" : "header__day"}>목</span>
+                <span className={today == "금요일" ? "header__today" : "header__day"}>금</span>
             </div>
             <div className="schedule-container__content">
                 <div className="schedule__frame">
@@ -18,7 +27,6 @@ export default function Schedule() {
                     <ScheduleColumn />
                     <ScheduleColumn />
                     <ScheduleColumn />
-                    
                 </div>
             </div>
         </div>
