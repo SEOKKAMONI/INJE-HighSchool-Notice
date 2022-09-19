1cba17db4911cb62f27c1027d8ce3fa3e5f0312f
unknown <sj01048260279@gmail.com>
2022-09-19 19:10:13 +0900

    ADD :: Schedule-header - 요일에 따라 그 요일 파란색으로 강조시켜주는거 아직 못함 - 레이아웃은 완성

--- components/Schedule/Schedule.js
+++ components/Schedule/Schedule.js
@@ -3,7 +3,13 @@ import "./Schedule.css";
 export default function Schedule() {
     return (
         <div className="schedule-container">
-            
+            <div className="schedule-container__header">
+                <span>월</span>
+                <span>화</span>
+                <span>수</span>
+                <span>목</span>
+                <span>금</span>
+            </div>
         </div>
     )
 }
