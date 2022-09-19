f5a3984e4659b84cf431d5bfb3f4808b63735886
unknown <sj01048260279@gmail.com>
2022-09-19 12:15:07 +0900

    ADD :: Main - Main.js 생성 - Main.css 생성 - main-content 레이아웃

---
+++ components/Main/Main.js
@@ -0,0 +1,12 @@
+import "./Main.css";
+
+export default function Main() {
+    return (
+        <div className="main-container">
+            {/* 시간표, 급식표 나오는 부분 */}
+            <div className="main-content">
+
+            </div>
+        </div>
+    )
+}
