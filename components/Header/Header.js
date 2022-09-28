702aa5979ca93207e67e5ebeb73140444cfb7690
SEOKKAMONI <sj01048260279@gmail.com>
2022-09-28 13:34:40 +0900

    ADD :: Header Logo 추가

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -3,7 +3,9 @@ import "./Header.css"
 export default function Header() {
     return (
         <div className="header">
-            
+            <div className="header__logo">
+                인제알리미
+            </div>
         </div>
     )
 }
