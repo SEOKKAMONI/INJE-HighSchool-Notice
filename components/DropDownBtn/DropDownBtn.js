489f72050bb7468d5369baaffcbe785092c954c1
SEOKKAMONI <sj01048260279@gmail.com>
2022-09-28 15:31:15 +0900

    ADD :: Drop Down Btn Onclick 이벤트

--- components/DropDownBtn/DropDownBtn.js
+++ components/DropDownBtn/DropDownBtn.js
@@ -3,14 +3,20 @@ import "./DropDownBtn.css";
 import arrowSvg from "../../img/arrow.svg"
 
 export default function DropDownBtn() {
+    const [showDrop, setShowDrop] = useState(false);
+    const ShowDropDownItems = () => {
+        setShowDrop(!showDrop)
+    }
     return (
         <div className="drop-down-menu">
-            <button className="drop-down-btn">
+            <button className="drop-down-btn"
+                onClick={() => ShowDropDownItems()}
+            >
                 <span className="grade-item">1학년</span>
                 <img src={arrowSvg} className="arrowSvg" />
             </button>
             <ul
-                className="drop-down-items"
+                className={showDrop ? "drop-down-items" : "drop-down-items drop-down-hidden"}
             >
                 <li className="drop-down-item">
                     <span>1학년</span>
