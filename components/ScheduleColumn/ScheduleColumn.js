5b0afa72acc035cf292fa1f822dade20479dd877
SEOKKAMONI <sj01048260279@gmail.com>
2022-10-01 16:21:10 +0900

    ADD :: Schedule 필터링 추가:

--- components/ScheduleColumn/ScheduleColumn.js
+++ components/ScheduleColumn/ScheduleColumn.js
@@ -2,25 +2,27 @@ import "./ScheduleColumn.css"
 import { useState, useEffect } from "react";
 import axios from 'axios';
 
-export default function ScheduleColumn({URL, GRADE_DATE, CLASS_NM_DATA, ALL_TI_YMD_DATA}) {
+export default function ScheduleColumn({ URL, GRADE_DATE, CLASS_NM_DATA, ALL_TI_YMD_DATA }) {
     const [schedules, setSchedule] = useState(null);
+    let HTML_CODE = ""
+    let ScheduleList = ""
 
     useEffect(() => {
         const ScheduleData = async () => {
-          try {
-            const response = await axios.get(
-              `${URL}`, {
-                params: {
-                    GRADE: GRADE_DATE,
-                    CLASS_NM: CLASS_NM_DATA,
-                    ALL_TI_YMD: ALL_TI_YMD_DATA
+            try {
+                const response = await axios.get(
+                    `${URL}`, {
+                    params: {
+                        GRADE: GRADE_DATE,
+                        CLASS_NM: CLASS_NM_DATA,
+                        ALL_TI_YMD: ALL_TI_YMD_DATA
+                    }
                 }
-              }
-            );
-            setSchedule(response.data.misTimetable[1].row)
-          } catch (error) {
-            console.log(error)
-          }
+                );
+                setSchedule(response.data.misTimetable[1].row)
+            } catch (error) {
+                console.log(error)
+            }
         };
         ScheduleData();
         console.log(schedules)
@@ -28,11 +30,26 @@ export default function ScheduleColumn({URL, GRADE_DATE, CLASS_NM_DATA, ALL_TI_Y
 
     return (
         <div>
-            <div className="schedule__column">
-               {schedules && schedules.map((schedule, index) => (
-                    <span key={index}>{schedule.ITRT_CNTNT.replace('-', '')}</span>
-                ))}
-            </div>
+            {schedules && schedules.map(function (schedule, index) {
+                if (schedule.ITRT_CNTNT == "-기술·가정") {
+                    ScheduleList += `<span>기가</span>`
+                } else if (schedule.ITRT_CNTNT == "-(자)예술·체육활동") {
+                    ScheduleList += `<span>예체능</span>`
+                } else if (schedule.ITRT_CNTNT == "-(창)동아리활동" || schedule.ITRT_CNTNT == "-(자)동아리활동") {
+                    ScheduleList += `<span>동아리</span>`
+                } else if (schedule.ITRT_CNTNT == "-진로와 직업") {
+                    ScheduleList += `<span>진로</span>`
+                } else if (schedule.ITRT_CNTNT == "-(자)진로탐색활동") {
+                    ScheduleList += `<span>진탐</span>`
+                } else {
+                    ScheduleList += `<span>${schedule.ITRT_CNTNT.replace("-", "")}</span>`
+                }
+                if (index == 5) {
+                    HTML_CODE = ScheduleList;
+                    ScheduleList = ""
+                    return <div className="schedule__column" dangerouslySetInnerHTML={{__html: HTML_CODE}}></div>
+                }
+            })}
         </div>
     )
 }
